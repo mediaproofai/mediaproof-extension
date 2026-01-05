261e5ee6f4e63021414fa0e64714795753ffbf34
mediaproofai <mediaproofai@gmail.com>
2026-01-05 21:02:18 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    id: "verify-image",
+    id: "mediaproof-verify",
     title: "Verify with Mediaproof",
     contexts: ["image"]
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "verify-image") {
+  if (info.menuItemId === "mediaproof-verify") {
     chrome.tabs.sendMessage(tab.id, {
       type: "VERIFY_IMAGE",
       imageUrl: info.srcUrl
